9c55f1f4856cd19313488ad8999b45be1bfa1c36
이세진 <leesejinn@naver.com>
2022-07-12 16:21:45 +0900

    feat: ERD에 맞춘 도메인 작성
    - Account, Article, Comment 추가

---
+++ src/main/java/com/sedin/qna/article/model/Article.java
@@ -0,0 +1,45 @@
+package com.sedin.qna.article.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Article")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "article_id")
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
+
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "modified_by", nullable = false)
+    private String modifiedBy;
+}
