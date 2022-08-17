a6d2af8e459d41ecf4f7a166940fc7b1e0e97ccb
이세진 <sedin@kakao.com>
2022-08-17 15:29:23 +0900

    test: AccountController Post 요청 성공 시나리오
    - Post 요청으로 사용자 생성 요청시 HttpStatus 201 Created 응답

---
+++ src/test/java/com/sedin/qna/account/controller/AccountControllerWebTest.java
@@ -0,0 +1,110 @@
+package com.sedin.qna.account.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sedin.qna.account.model.dto.AccountSignUpDto;
+import com.sedin.qna.account.model.response.AccountApiResponse;
+import com.sedin.qna.account.service.AccountService;
+import com.sedin.qna.network.Header;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AccountController.class)
+class AccountControllerWebTest {
+
+    private static final Long EXISTED_ID = 1L;
+    private static final String LOGIN_ID = "sedin";
+    private static final String PASSWORD = "12341234";
+    private static final String NAME = "LeeSeJin";
+    private static final String SEX = "M";
+    private static final String EMAIL = "sejin@email.com";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private AccountService accountService;
+
+    private Header<AccountApiResponse> response;
+
+    @BeforeEach
+    void prepare() {
+        AccountApiResponse accountApiResponse = AccountApiResponse.builder()
+                .id(EXISTED_ID)
+                .loginId(LOGIN_ID)
+                .name(NAME)
+                .bornDate(LocalDateTime.now())
+                .sex(SEX)
+                .email(EMAIL)
+                .build();
+
+        response = Header.OK(accountApiResponse);
+    }
+
+    @Nested
+    @DisplayName("POST /api/accounts 는")
+    class DescribeSignUpAccount {
+
+        @Nested
+        @DisplayName("사용자 등록 요청이 들어오면")
+        class ContextWithAccountSignUpDto {
+
+            @BeforeEach
+            void prepareAccountSignUpDto() {
+                given(accountService.signUp(any(AccountSignUpDto.class)))
+                        .willReturn(response);
+            }
+
+            @Test
+            @DisplayName("HttpStatus 201 Created를 응답한다")
+            void it_returns_httpStatus_created() throws Exception {
+                AccountSignUpDto accountSignUpDto = AccountSignUpDto.builder()
+                        .loginId(LOGIN_ID)
+                        .password(PASSWORD)
+                        .name(NAME)
+                        .bornDate(LocalDateTime.now())
+                        .sex(SEX)
+                        .email(EMAIL)
+                        .build();
+
+                String content = objectMapper.writeValueAsString(accountSignUpDto);
+
+                mockMvc.perform(post("/api/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                        .andExpect(status().isCreated())
+                        .andExpect(content().string(containsString(LOGIN_ID)));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("PATCH /api/accounts/{id} 는")
+    class DescribeUpdateAccount {
+
+    }
+
+    @Nested
+    @DisplayName("DELETE /api/accounts/{id} 는")
+    class DescribeDeleteAccount {
+
+    }
+}
