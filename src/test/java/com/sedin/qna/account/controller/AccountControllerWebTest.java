4ba5d866143c30d2f667d7af7962de033a77649c
이세진 <sedin@kakao.com>
2022-08-18 15:53:26 +0900

    test: Account 이미 등록된 정보로 회원가입 시나리오
    - loginId, email이 이미 등록되어 있으면 400 Bad Request를 응답한다

--- src/test/java/com/sedin/qna/account/controller/AccountControllerWebTest.java
+++ src/test/java/com/sedin/qna/account/controller/AccountControllerWebTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sedin.qna.account.model.dto.AccountSignUpDto;
 import com.sedin.qna.account.model.response.AccountApiResponse;
 import com.sedin.qna.account.service.AccountService;
+import com.sedin.qna.error.DuplicatedException;
 import com.sedin.qna.network.Header;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,6 +21,8 @@ import java.time.LocalDateTime;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,10 +32,12 @@ class AccountControllerWebTest {
 
     private static final Long EXISTED_ID = 1L;
     private static final String LOGIN_ID = "sedin";
+    private static final String EXISTED_LOGIN_ID = "sedin";
     private static final String PASSWORD = "12341234";
     private static final String NAME = "LeeSeJin";
     private static final String SEX = "M";
     private static final String EMAIL = "sejin@email.com";
+    private static final String EXISTED_EMAIL = "sejin@email.com";
 
     @Autowired
     private MockMvc mockMvc;
@@ -43,6 +48,7 @@ class AccountControllerWebTest {
     @MockBean
     private AccountService accountService;
 
+    private AccountSignUpDto accountSignUpDto;
     private Header<AccountApiResponse> response;
 
     @BeforeEach
@@ -76,7 +82,7 @@ class AccountControllerWebTest {
             @Test
             @DisplayName("HttpStatus 201 Created를 응답한다")
             void it_returns_httpStatus_created() throws Exception {
-                AccountSignUpDto accountSignUpDto = AccountSignUpDto.builder()
+                accountSignUpDto = AccountSignUpDto.builder()
                         .loginId(LOGIN_ID)
                         .password(PASSWORD)
                         .name(NAME)
@@ -92,6 +98,41 @@ class AccountControllerWebTest {
                         .content(content))
                         .andExpect(status().isCreated())
                         .andExpect(content().string(containsString(LOGIN_ID)));
+
+                verify(accountService).signUp(any(AccountSignUpDto.class));
+            }
+        }
+
+        @Nested
+        @DisplayName("이미 등록된 정보로 요청이 들어오면")
+        class ContextWithAlreadyRegisteredAccountSignUpDto {
+
+            @BeforeEach
+            void prepareRegisteredDto() {
+                given(accountService.signUp(any(AccountSignUpDto.class)))
+                        .willThrow(new DuplicatedException(EXISTED_LOGIN_ID));
+            }
+
+            @Test
+            @DisplayName("HttpStatus 400 BadRequest를 응답한다")
+            void it_returns_httpStatus_badRequest() throws Exception {
+                accountSignUpDto = AccountSignUpDto.builder()
+                        .loginId(EXISTED_LOGIN_ID)
+                        .password(PASSWORD)
+                        .name(NAME)
+                        .bornDate(LocalDateTime.now())
+                        .sex(SEX)
+                        .email(EMAIL)
+                        .build();
+
+                String content = objectMapper.writeValueAsString(accountSignUpDto);
+
+                mockMvc.perform(post("/api/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                        .andExpect(status().isBadRequest());
+
+                verify(accountService, times(1)).signUp(any(AccountSignUpDto.class));
             }
         }
     }
