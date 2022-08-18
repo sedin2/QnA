b01d2ac2c351c93eeb8ad6ece270991d682281b0
이세진 <sedin@kakao.com>
2022-08-18 16:34:23 +0900

    feat: InvalidArgumentErrorAdvice 추가
    - MethodArgumentNotValidException 예외 처리

---
+++ src/main/java/com/sedin/qna/common/InvalidArgumentErrorAdvice.java
@@ -0,0 +1,45 @@
+package com.sedin.qna.common;
+
+import com.sedin.qna.network.Header;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 유효성 검사 관련 예외처리를 담당합니다.
+ */
+@RestControllerAdvice
+public class InvalidArgumentErrorAdvice {
+
+    private static final String ERROR_MESSAGE = "Argument Not Valid";
+
+    /**
+     * 요청 한 메소드의 인자값이 유효하지 않을 때 에러 메세지를 리턴합니다.
+     *
+     * @return 에러 메세지
+     */
+    /**
+     * 요청 한 메소드의 인자값이 유효하지 않을 때 에러 메세지를 리턴합니다.
+     * @param exception 인자값 예외
+     * @return 에러 메세지
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Header<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+        Map<String, String> errorMap = new HashMap<>();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return Header.ERROR(ERROR_MESSAGE, errorMap);
+    }
+}
