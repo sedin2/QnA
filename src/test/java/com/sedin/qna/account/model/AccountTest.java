7ed12c8030b869b69aebe202d1b4d94634dd7e92
이세진 <sedin@kakao.com>
2022-08-22 18:55:34 +0900

    test: Account Update하는 부분 테스트 추가
    - 사용자 정보를 update하는 부분 테스트

--- src/test/java/com/sedin/qna/account/model/AccountTest.java
+++ src/test/java/com/sedin/qna/account/model/AccountTest.java
@@ -8,7 +8,6 @@ class AccountTest {
 
     @Test
     void When_New_Account_Expect_Success() {
-
         // given
         final String LOGIN_ID = "sedin";
         final String PASSWORD = "1234";
@@ -25,5 +24,26 @@ class AccountTest {
         assertThat(account.getId()).isEqualTo(1L);
         assertThat(account.getLoginId()).isEqualTo(LOGIN_ID);
         assertThat(account.getPassword()).isEqualTo(PASSWORD);
+    }
+
+    @Test
+    void When_Update_Account_Expect_Success() {
+        // given
+        final String PASSWORD = "password";
+        final String NEW_PASSWORD = "newPassword";
+        final String EMAIL = "email@email.com";
+        final String NEW_EMAIL = "newEmail@email.com";
+
+        // when
+        Account account = Account.builder()
+                .password(PASSWORD)
+                .email(EMAIL)
+                .build();
+
+        account.updatePasswordAndEmail(NEW_PASSWORD, NEW_EMAIL);
+
+        // then
+        assertThat(account.getPassword()).isEqualTo(NEW_PASSWORD);
+        assertThat(account.getEmail()).isEqualTo(NEW_EMAIL);
     }
 }
