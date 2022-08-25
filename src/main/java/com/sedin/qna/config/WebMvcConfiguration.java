ab90a5f52725b7f7c3ef8233e3fb97b822a1fd53
이세진 <sedin@kakao.com>
2022-08-25 17:30:39 +0900

    feat: WebMvcConfigurer의 addResourceHandlers 메소드 오버라이드
    - '/api/docs/index.html' 같이 '/api/docs/**' 형식의 url을 classpath 하위의 /static/docs/ 디렉토리 아래의 파일을 찾아 반환

---
+++ src/main/java/com/sedin/qna/config/WebMvcConfiguration.java
@@ -0,0 +1,14 @@
+package com.sedin.qna.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/docs/**").addResourceLocations("classpath:/static/docs/");
+    }
+}
