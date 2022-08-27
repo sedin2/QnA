78994e334379769a6f874b087732dd3df5fbfda5
이세진 <sedin@kakao.com>
2022-08-27 21:27:25 +0900

    feat: 인증 관련 advice, exception 추가
    - Password가 틀릴 때 던지는 PasswordNotCorrectException 작성
    - 인증 관련 예외처리 AuthorizedErrorAdvice 작성

--- src/main/java/com/sedin/qna/exception/InvalidTokenException.java
+++ src/main/java/com/sedin/qna/exception/InvalidTokenException.java
@@ -1,6 +1,10 @@
 package com.sedin.qna.exception;
 
+/**
+ * 토큰이 유효하지 않은 경우에 던집니다.
+ */
 public class InvalidTokenException extends RuntimeException {
+
     public InvalidTokenException(String token) {
         super("Invalid token: " + token);
     }
--- src/main/java/com/sedin/qna/exception/NotFoundException.java
+++ src/main/java/com/sedin/qna/exception/NotFoundException.java
@@ -1,6 +1,10 @@
 package com.sedin.qna.exception;
 
+/**
+ * 리소스를 찾을 수 없을 때 던집니다.
+ */
 public class NotFoundException extends RuntimeException {
+
     public NotFoundException(String message) {
         super(message);
     }
