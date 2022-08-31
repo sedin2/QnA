68fca590cea0db724cb5579f81f2593d72e46548
이세진 <sedin@kakao.com>
2022-08-31 21:22:59 +0900

    feat: 인증 인터셉터 경로 추가 및 기능 구현
    - 인증이 필요없는 경로 추가 해주고, access token pasing하는 로직 추가

--- src/main/java/com/sedin/qna/interceptor/AuthenticationInterceptor.java
+++ src/main/java/com/sedin/qna/interceptor/AuthenticationInterceptor.java
@@ -1,7 +1,7 @@
 package com.sedin.qna.interceptor;
 
+import com.sedin.qna.athentication.service.AuthenticationService;
 import com.sedin.qna.exception.InvalidTokenException;
-import com.sedin.qna.util.JwtUtil;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -16,10 +16,10 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
     private final String AUTHORIZATION = "Authorization";
     private final String BEARER = "Bearer ";
 
-    private final JwtUtil jwtUtil;
+    private final AuthenticationService authenticationService;
 
-    public AuthenticationInterceptor(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
+    public AuthenticationInterceptor(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
     }
 
     @Override
@@ -30,10 +30,6 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
             return true;
         }
 
-        if (request.getRequestURI().startsWith("/api/auth/login") && request.getMethod().equals(HttpMethod.POST.toString())) {
-            return true;
-        }
-
         String authorization = request.getHeader(AUTHORIZATION);
 
         if (!authorization.startsWith(BEARER)) {
@@ -41,7 +37,7 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
         }
 
         String accessToken = authorization.substring(BEARER.length());
-        request.setAttribute(ACCOUNT_ID, jwtUtil.decode(accessToken).get(ACCOUNT_ID));
+        request.setAttribute(ACCOUNT_ID, authenticationService.decodeAccessToken(accessToken));
 
         return true;
     }
