208bb4bd37bac2d8eb40868045aa4174342078f5
이세진 <sedin@kakao.com>
2022-09-07 20:44:15 +0900

    refactor: OpenEntityManagerInViewInterceptor 빈 제거
    - AuthenticationInterceptor 등록 할 때, order를 최 하위로 주어 제일 마지막으로 등록

--- src/main/java/com/sedin/qna/config/WebConfigInterceptors.java
+++ src/main/java/com/sedin/qna/config/WebConfigInterceptors.java
@@ -1,27 +1,22 @@
 package com.sedin.qna.config;
 
 import com.sedin.qna.interceptor.AuthenticationInterceptor;
-import org.springframework.orm.jpa.support.OpenEntityManagerInViewInterceptor;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfigInterceptors implements WebMvcConfigurer {
 
-    private final OpenEntityManagerInViewInterceptor openEntityManagerInViewInterceptor;
     private final AuthenticationInterceptor authenticationInterceptor;
 
-    public WebConfigInterceptors(OpenEntityManagerInViewInterceptor openEntityManagerInViewInterceptor,
-                                 AuthenticationInterceptor authenticationInterceptor) {
-        this.openEntityManagerInViewInterceptor = openEntityManagerInViewInterceptor;
+    public WebConfigInterceptors(AuthenticationInterceptor authenticationInterceptor) {
         this.authenticationInterceptor = authenticationInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addWebRequestInterceptor(openEntityManagerInViewInterceptor)
-                .addPathPatterns("/**")
-                .order(0);
         registry.addInterceptor(authenticationInterceptor)
-                .excludePathPatterns("/favicon.ico", "/api/docs/*", "/api/auth/*");
+                .excludePathPatterns("/favicon.ico", "/api/docs/*", "/api/auth/*")
+                .order(Ordered.LOWEST_PRECEDENCE);
     }
 }
