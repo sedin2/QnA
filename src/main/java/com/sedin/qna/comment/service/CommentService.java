3bace7422f063fc7da6c36f7778d3f56560c4d39
이세진 <sedin@kakao.com>
2022-09-13 20:06:02 +0900

    feat: CommentService 작성
    - Comment 관련 서비스 로직 작성

---
+++ src/main/java/com/sedin/qna/comment/service/CommentService.java
@@ -0,0 +1,19 @@
+package com.sedin.qna.comment.service;
+
+import com.sedin.qna.account.model.Account;
+import com.sedin.qna.comment.model.CommentDto;
+
+import java.util.List;
+
+public interface CommentService {
+
+    CommentDto.Response create(Account account, Long articleId, CommentDto.Create create);
+
+    List<CommentDto.Response> findAll(Long articleId);
+
+    CommentDto.Response findById(Long articleId, Long commentId);
+
+    CommentDto.Response update(Account account, Long articleId, Long commentId, CommentDto.Update update);
+
+    void delete(Account account, Long articleId, Long commentId);
+}
