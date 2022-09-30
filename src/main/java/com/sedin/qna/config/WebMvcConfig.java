8718512b5efa6da364a119ae52a91deec5db6da6
이세진 <sedin@kakao.com>
2022-09-30 20:47:15 +0900

    refactor: 배포 테스트를 위한 수정
    - 배포 테스트를 위한 수정사항

--- src/main/java/com/sedin/qna/config/WebMvcConfig.java
+++ src/main/java/com/sedin/qna/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.sedin.qna.config;
 import com.sedin.qna.interceptor.AuthenticationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,7 +19,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/api/docs/**")
-                .addResourceLocations("classpath:/static/docs/");
+                .addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/docs/");
     }
 
     @Bean
