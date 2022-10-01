4d44d82a0f197af26ee5b5795248108f730b1724
이세진 <sedin@kakao.com>
2022-10-01 09:58:54 +0900

    docs: 배포 스크립트 수정
    - 배포할때 쓰이는 jar경로 재설정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+JAR_FILE="$PROJECT_ROOT/build/libs/*.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
