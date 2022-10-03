cdad42e1799df40ab076573a841060947fe71b0b
이세진 <sedin@kakao.com>
2022-10-03 14:38:40 +0900

    refactor: 배포 스크립트 수정
    - 자동배포 쉘 스크립트 롤백

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/build/libs/*.jar"
+JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
+
+# build 파일 복사
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
