2ec2fc947d4aa341dab79881bae7c3174fff9122
이세진 <sedin@kakao.com>
2022-10-07 15:50:51 +0900

    feat: N+1 query - fetch join
    - N+1 query 해결을 위해 fetch join 사용

--- src/main/java/com/sedin/qna/article/repository/ArticleRepository.java
+++ src/main/java/com/sedin/qna/article/repository/ArticleRepository.java
@@ -1,8 +1,14 @@
 package com.sedin.qna.article.repository;
 
 import com.sedin.qna.article.model.Article;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
+    @Query("select a from Article a left join fetch a.comments")
+    List<Article> findAllFetchJoin(Pageable pageable);
 }
