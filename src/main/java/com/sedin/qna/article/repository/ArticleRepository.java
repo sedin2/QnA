66d17957fecdbc709a47c6366702ad46a915a08a
이세진 <sedin@kakao.com>
2022-10-08 19:44:14 +0900

    feat: N+1 query - entity graph
    - N+1 query 해결을 위해 entity graph 사용

--- src/main/java/com/sedin/qna/article/repository/ArticleRepository.java
+++ src/main/java/com/sedin/qna/article/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.sedin.qna.article.repository;
 
 import com.sedin.qna.article.model.Article;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,4 +12,8 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     @Query("select a from Article a left join fetch a.comments")
     List<Article> findAllFetchJoin(Pageable pageable);
+
+    @EntityGraph(attributePaths = "comments")
+    @Query("select a from Article a")
+    List<Article> findAllEntityGraph(Pageable pageable);
 }
