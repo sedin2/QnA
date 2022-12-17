0624877ed88de002dbdcef6512a908970ae0b526
이세진 <sedin@kakao.com>
2022-12-17 22:18:20 +0900

    refactor: Spring Security Configuration 설정파일 추가
    - Security 설정을 위한 Config 작성

---
+++ src/main/java/com/sedin/qna/config/SecurityConfiguration.java
@@ -0,0 +1,64 @@
+package com.sedin.qna.config;
+
+import com.sedin.qna.authentication.filter.JwtAuthenticationFilter;
+import com.sedin.qna.config.model.CustomAccessDeniedHandler;
+import com.sedin.qna.config.model.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity(debug = true)
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .csrf().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
+                .accessDeniedHandler(customAccessDeniedHandler)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/accounts", "/api/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/docs/**").permitAll()
+                .antMatchers("/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().disable();
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
