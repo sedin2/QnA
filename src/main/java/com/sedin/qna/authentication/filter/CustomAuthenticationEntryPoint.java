1d45fc18ea0c7b2b600bbbdbb911484e0e5dd52d
이세진 <sedin@kakao.com>
2022-12-18 18:36:38 +0900

    feat: Security 로직 구현
    - Filter, EntryPoint, AccessDeniedHandler 구현

---
+++ src/main/java/com/sedin/qna/authentication/filter/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package com.sedin.qna.authentication.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sedin.qna.common.response.ApiResponseCode;
+import com.sedin.qna.common.response.ApiResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        try (OutputStream os = response.getOutputStream()) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(os, ApiResponseDto.ERROR(ApiResponseCode.UNAUTHORIZED, authException.getMessage()));
+            os.flush();
+        }
+    }
+}
