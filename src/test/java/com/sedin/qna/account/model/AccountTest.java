849af0c9ae346fa2170b781eda9717ed54baf189
이세진 <sedin@kakao.com>
2022-12-20 16:41:37 +0900

    test: Account Unit Test 수정
    - Account Unit Test 수정

--- src/test/java/com/sedin/qna/account/model/AccountTest.java
+++ src/test/java/com/sedin/qna/account/model/AccountTest.java
@@ -1,48 +1,48 @@
 package com.sedin.qna.account.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("Account Unit Test")
 class AccountTest {
 
     @Test
+    @DisplayName("Account instance 생성")
     void When_New_Account_Expect_Success() {
         // given
-        final String LOGIN_ID = "sedin";
         final String PASSWORD = "1234";
 
         // when
         Account account = Account.builder()
-                .loginId(LOGIN_ID)
                 .password(PASSWORD)
                 .build();
 
         // then
         assertThat(account).isNotNull();
-        assertThat(account.getLoginId()).isEqualTo(LOGIN_ID);
         assertThat(account.getPassword()).isEqualTo(PASSWORD);
     }
 
     @Test
+    @DisplayName("updatePasswordAndName 메서드 테스트")
     void When_Update_Account_Expect_Success() {
         // given
         final String PASSWORD = "password";
         final String NEW_PASSWORD = "newPassword";
-        final String EMAIL = "email@email.com";
-        final String NEW_EMAIL = "newEmail@email.com";
+        final String NAME = "Mocha";
+        final String NEW_NAME = "CafeMocha";
 
         // when
         Account account = Account.builder()
                 .password(PASSWORD)
-                .email(EMAIL)
+                .name(NAME)
                 .build();
 
-        Account updated = account.updatePasswordAndEmail(NEW_PASSWORD, NEW_EMAIL);
+        account.updatePasswordAndName(NEW_PASSWORD, NEW_NAME);
 
         // then
-        assertThat(account).isEqualTo(updated);
         assertThat(account.getPassword()).isEqualTo(NEW_PASSWORD);
-        assertThat(account.getEmail()).isEqualTo(NEW_EMAIL);
+        assertThat(account.getName()).isEqualTo(NEW_NAME);
     }
 }
